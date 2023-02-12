95d4b54e49482a4ba6631317b89178707ea0f995
Saiful264 <saifulislamx264@gmail.com>
2023-02-12 10:39:07 +0600

    new page been create

---
+++ js/login.js
@@ -0,0 +1,20 @@
+// step 1: add click event handler with submit button
+
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //step-2: get the emild adderss insider the input field
+    // always remember to use .value to get text form an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    //step-3: get password
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    // DO NOT VERIFY email passworld n cliend side
+    // step 4: verify email and password
+    if (email === "saiful@gmail.con" && password === "1234sa") {
+        console.log('valid user');
+    }else{
+        console.log('Invalid user');
+    }
+
+})
